2766f1bb352907600fcb3e6064fd39c8f3dd1e5a
emmajiugo <emmajiugo@gmail.com>
2019-11-26 22:55:56 +0100

    Skooleo Mobile

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,9 @@
 |
 */
 
-// $router->get('/', function () use ($router) {
-//     return $router->app->version();
-// });
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
 
 Route::group(['prefix' => 'api/v1'], function () use ($router) {
 
