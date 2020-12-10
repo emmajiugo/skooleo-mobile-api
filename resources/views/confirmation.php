8933ce7bf96cfc465e3f6c91d104e390bb48a0c1
emmajiugo <emmajiugo@gmail.com>
2020-12-10 08:42:06 +0100

    debugging confirmation view page

--- resources/views/confirmation.php
+++ resources/views/confirmation.php
@@ -3,7 +3,7 @@
 <head>
     <!-- CSS only -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,600,700,800,900" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
         body {
