f256d281f8663d86301177ec844417cc98f0ba68
emmajiugo <emmajiugo@gmail.com>
2021-01-05 21:35:59 +0100

    debugging redirect after payment

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class HttpsProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!$request->secure() && Str::startsWith(config('app.url'), 'https://')) {
+            return redirect()->secure($request->getRequestUri());
+        }
+        return $next($request);
+    }
+}
